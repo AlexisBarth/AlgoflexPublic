f3b0356c5eef7946e0a6a8af2dc0bb509d8ecd9a
Alexis <alexis.barthelmebs@epitech.eu>
2021-06-18 02:25:20 +0200

    Preview IDE
    Visuel fonctionnel

--- algoflex_frontend/src/index.tsx
+++ algoflex_frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Ide from './component/ide/ide';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Ide />
   </React.StrictMode>,
   document.getElementById('root')
 );

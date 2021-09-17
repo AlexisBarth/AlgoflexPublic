3abda454e36d3a114c1b1d5f2452927e80da33b6
Hugo Haquette <hugo097@hotmail.fr>
2021-09-17 14:38:34 +0200

    AL-28 : Suppression du code inutile

--- algoflex_backend/src/app.ts
+++ algoflex_backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes';
 const app = express();
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
--- algoflex_backend/src/routes/index.ts
+++ algoflex_backend/src/routes/index.ts
@@ -1,9 +1,5 @@
 import express from 'express'
-import server from '../httpServer';
-import * as WebSocket from 'ws';
-import { BuildListener } from '../listeners/build_listener';
 
 const router = express.Router();
-
 
 export default router;

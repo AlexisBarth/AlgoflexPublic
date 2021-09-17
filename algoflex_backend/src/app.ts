cbb4bd2e3ad2805ee1918a2f91d20ef3356aa4b1
Pakome <maxime.lardier@epitech.eu>
2021-09-17 15:54:37 +0200

    fix: configure typeorm

--- algoflex_backend/src/app.ts
+++ algoflex_backend/src/app.ts
@@ -1,11 +1,18 @@
 import "reflect-metadata";
 import { createExpressServer } from 'routing-controllers';
 import { UserController } from './controllers/user.controller';
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
+const ormConfig = require('../ormconfig.json');
 
 const app = createExpressServer({
   controllers: [
     UserController
   ],
 });
+
+createConnection(ormConfig).then(connection => {
+  console.log("Sucessfully connected to DB");
+}).catch(error => console.log(error));
 
 export default app;

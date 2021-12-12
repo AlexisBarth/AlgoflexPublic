cbf2236e5274ac67ccc3f887cf6f4c7734999ca8
PichardN <nicolas.pichard@epitech.eu>
2021-12-12 21:27:51 +0100

    AL-51 NavBar: Basic setup with routes and user display

--- algoflex_frontend/src/components/Navbar/Navbar.tsx
+++ algoflex_frontend/src/components/Navbar/Navbar.tsx
@@ -1,35 +1,62 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Logout } from '@components';
 import IsAuthenticated from '@services/Authentication.utils';
+import {Toolbar, Typography, Divider} from '@mui/material'
+import {useContext} from "react";
+import {FirebaseContext} from "@services/Firebase";
 
 const Navbar = () => {
+    const firebase = useContext(FirebaseContext);
+
+    const AuthContent = () => {
+        if (IsAuthenticated()){
+            return (<>
+                <Typography variant="h6" component={Link} to='editeur'
+                            style={{color: 'inherit', textDecoration: 'none', margin: 15}}>
+                    Editeur
+                </Typography>
+            </>);
+        }
+        return (<></>)
+    }
 
     const AuthCheck = () => {
         if(IsAuthenticated()){
+            //TODO : change link to --> user page
+            //TODO : ask that firebase auth save username, not only email
             return (<>
-                    <NavLink exact to="editeur" activeClassName='nav-active'>
-                        Editeur
-                    </NavLink>
-                    <Logout />
-                </>);
+                <Typography variant="h6" component={Link} to='about' style={{ color: 'inherit', textDecoration: 'none', margin: 15 }}>
+                    {firebase.auth.currentUser?.email}
+                </Typography>
+                <Typography variant="h6" component={Logout} style={{ color: 'inherit', textDecoration: 'none', margin: 15 }}>
+                    Logout
+                </Typography>
+            </>);
         }
         return(<>
-                <NavLink exact to="login" activeClassName="nav-active">
-                    Connexion
-                </NavLink> 
+            <Typography variant="h6" component={Link} to='login' style={{ color: 'inherit', textDecoration: 'none', margin: 15 }}>
+                Log in
+            </Typography>
+            <Divider orientation="vertical" variant="middle" flexItem style={{ background: 'black', width: "2.5px"}}/>
+            <Typography variant="h6" component={Link} to='login' style={{ color: 'inherit', textDecoration: 'none', margin: 15 }}>
+                Sign up
+            </Typography>
             </>);
     }
 
     return (
-        <div className="navigation">
-            <NavLink exact to="/" activeClassName="nav-active">
-                Accueil
-            </NavLink>
-            <NavLink exact to="about" activeClassName="nav-active">
-                A propos
-            </NavLink>
-            <AuthCheck />
-        </div>
+        <Toolbar className="navigation" style={{ display: 'flex' }}>
+            <div style={{display:'flex', flexGrow:1}}>
+                <Typography variant="h6" component={Link} to='/' style={{ color: 'inherit', textDecoration: 'none', margin: 15 }}>
+                    Accueil
+                </Typography>
+                <Typography variant="h6" component={Link} to='about' style={{ color: 'inherit', textDecoration: 'none', margin: 15 }}>
+                    A propos
+                </Typography>
+                <AuthContent/>
+            </div>
+            <AuthCheck/>
+        </Toolbar>
     );
 };
 

08923c989823509bb09bd97ceb0961ecb0801b72
Maxime Lardier <36198537+Pakome@users.noreply.github.com>
2022-01-14 17:28:15 +0100

    chore: migrate auth to cookie. AL-47 (#11)

--- algoflex_frontend/src/services/Axios.client.ts
+++ algoflex_frontend/src/services/Axios.client.ts
@@ -3,7 +3,5 @@ import axios from "axios";
 export const client = axios.create({
   baseURL: 'http://localhost:4100',
   timeout: 1000,
-  headers: {
-   // 'Authorization': `Bearer ${token}`,
-  }
+  withCredentials: true,
 });

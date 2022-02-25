9d76c58975fc2247d867b06511fc146d3d83cd97
Hugo Haquette <hugo097@hotmail.fr>
2022-02-25 10:19:53 +0100

    AL-54: Fix commentaires

--- algoflex_backend/src/gateways/compilation/build.gateway.ts
+++ algoflex_backend/src/gateways/compilation/build.gateway.ts
@@ -24,52 +24,54 @@ export class BuildGateway implements OnGatewayDisconnect, OnGatewayConnection {
 
   @SubscribeMessage('compile-request')
   async handleCompileRequest(client: any, event: compileRequestEvent): Promise<void> {
-    if (this.buildListener === null) {
-      this.buildListener = await BuildListener.create(
-        event.code,
-        `
-      {
-        "exercice": [
-            {
-                "entree": ["30"],
-                "sorties": ["a"],
-                "maxtime": 30000,
-                "maxmemory": 10
-            },
-            {
-                "entree": ["8"],
-                "sorties": ["a"],
-                "maxtime": 30000,
-                "maxmemory": 10
-            }
-        ]
-    }`
-      ); // TODO : a changer
-      this.execute = event.execute;
-
-      client.send(
-        JSON.stringify({
-          state: 1,
-          executeLink: this.buildListener.getExecuteLink(),
-          compileLink: this.buildListener.getCompileLink(),
-        })
-      );
+    if (this.buildListener !== null) {
+      return;
     }
+    this.buildListener = await BuildListener.create(
+      event.code,
+      `
+    {
+      "exercice": [
+          {
+              "entree": ["30"],
+              "sorties": ["a"],
+              "maxtime": 30000,
+              "maxmemory": 10
+          },
+          {
+              "entree": ["8"],
+              "sorties": ["a"],
+              "maxtime": 30000,
+              "maxmemory": 10
+          }
+      ]
+  }`
+    ); // TODO : a changer
+    this.execute = event.execute;
+
+    client.send(
+      JSON.stringify({
+        state: 1,
+        executeLink: this.buildListener.getExecuteLink(),
+        compileLink: this.buildListener.getCompileLink(),
+      })
+    );
   }
 
   @SubscribeMessage('execute-request')
   async handle(client: any): Promise<void> {
-    if (this.buildListener !== null) {
-      const hasCompiled = await this.buildListener.build();
-
-      client.send(JSON.stringify({ state: 2, hasCompiled }));
-
-      if (hasCompiled && this.execute) {
-        await this.buildListener.execute();
-        client.send(JSON.stringify({ state: 3, hasExecuted: this.buildListener.isExecuted() }));
-      }
-      this.removeDocker();
+    if (this.buildListener === null) {
+      return;
     }
+    const hasCompiled = await this.buildListener.build();
+
+    client.send(JSON.stringify({ state: 2, hasCompiled }));
+
+    if (hasCompiled && this.execute) {
+      await this.buildListener.execute();
+      client.send(JSON.stringify({ state: 3, hasExecuted: this.buildListener.isExecuted() }));
+    }
+    this.removeDocker();
   }
 
   handleDisconnect(_client: WebSocket) {

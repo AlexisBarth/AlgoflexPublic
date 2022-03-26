d35786de0baea990a1898d76e992f89d411c24ab
Hugo087 <hugo097@hotmail.fr>
2022-03-26 14:59:05 +0100

    AL-73: Fix craco build

--- algoflex_frontend/craco.config.js
+++ algoflex_frontend/craco.config.js
@@ -2,6 +2,18 @@ const CracoAlias = require("craco-alias");
 
 module.exports = {
   webpack: {
+    configure: {
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes("node_modules") &&
+            warning.details &&
+            warning.details.includes("source-map-loader")
+          );
+        },
+      ],
+    },
     alias: {
         'vscode': require.resolve('@codingame/monaco-languageclient/lib/vscode-compatibility')
     }

72f36cb51fd318c4796665231c86775a475fcc7a
Maxime Lardier <maxime.lardier@gmail.com>
2022-03-26 14:26:35 +0100

    test(back): add user uid to the submission. AL-68

--- algoflex_backend/src/gateways/build.module.ts
+++ algoflex_backend/src/gateways/build.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { BuildGateway } from './compilation/build.gateway';
-import { AutoCompleteGateway } from './autocomplete/autocomplete.gateway';
+import { AuthModule } from 'src/auth/auth.module';
 import { CodingQuestionsService } from 'src/problems/coding-questions/coding-questions.service';
 import { CodingQuestion } from 'src/problems/coding-questions/entities/coding-question.entity';
-import { Submission } from 'src/problems/submissions/entities/submission.entity';
 import { SubmissionsService } from 'src/problems/submissions/submissions.service';
+import { Submission } from 'src/problems/submissions/entities/submission.entity';
+import { User } from 'src/users/entity';
+import { BuildGateway } from './compilation/build.gateway';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CodingQuestion, Submission]),
+    AuthModule,
+    TypeOrmModule.forFeature([CodingQuestion, Submission, User]),
   ],
   providers: [
-    AutoCompleteGateway,
     BuildGateway,
     CodingQuestionsService,
     SubmissionsService,

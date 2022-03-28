345b796e9965ae8f7a17ac61fe5cc93c18a7e2ac
AlexisBarth <alexis.barthelmebs@epitech.eu>
2022-03-28 17:04:45 +0200

    Cleaned tests

--- algoflex_frontend/src/views/Exercices/components/Exercices.test.tsx
+++ algoflex_frontend/src/views/Exercices/components/Exercices.test.tsx
@@ -5,6 +5,16 @@ import { act } from "react-dom/test-utils";
 
 test.todo("Make Exercices tests");
 
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+    useParams: () => ({
+      pageId: "1234"
+    }),
+    useRouteMatch: () => ({ url: 'http://localhost:3000/theme/1234' }),
+  }));
+
+
 it("Renders correctly", () => {
     const { queryByTest } = render(<Exercices />);
     expect(queryByTest).toMatchSnapshot();

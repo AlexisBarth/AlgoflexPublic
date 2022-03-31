d5ec7c594524a475f4863c72b0a278d739a9d4f0
PichardN <nicolas.pichard@epitech.eu>
2022-03-31 16:15:01 +0200

    Update webpack config

--- algoflex_frontend/craco.config.js
+++ algoflex_frontend/craco.config.js
@@ -2,7 +2,7 @@ const CracoAlias = require("craco-alias");
 
 module.exports = {
   webpack: {
-    configure: {
+    options: {
       ignoreWarnings: [
         function ignoreSourcemapsloaderWarnings(warning) {
           return (

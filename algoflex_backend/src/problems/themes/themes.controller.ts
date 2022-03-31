d3c22d4a36078cd6a6902ed80e8e5a4e22a0b9d1
Maxime Lardier <36198537+Pakome@users.noreply.github.com>
2022-03-31 12:44:38 +0200

    chore(back): secure themes routes (#36)

--- algoflex_backend/src/problems/themes/themes.controller.ts
+++ algoflex_backend/src/problems/themes/themes.controller.ts
@@ -1,15 +1,18 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 
 import { ThemesService } from './themes.service';
 import { CreateThemeDto } from './dto/create-theme.dto';
 import { UpdateThemeDto } from './dto/update-theme.dto';
+import { FirebaseAuthGuard, RolesGuard, Roles, Role } from 'src/common';
 
 @ApiTags('Themes')
 @Controller('problems/themes')
 export class ThemesController {
   constructor(private readonly themesService: ThemesService) {}
 
+  @UseGuards(FirebaseAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Post()
   public async create(@Body() createThemeDto: CreateThemeDto) {
     return this.themesService.create(createThemeDto);
@@ -25,11 +28,15 @@ export class ThemesController {
     return this.themesService.findOne(id);
   }
 
+  @UseGuards(FirebaseAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Put(':id')
   public async update(@Param('id') id: string, @Body() updateThemeDto: UpdateThemeDto) {
     return this.themesService.update(+id, updateThemeDto);
   }
 
+  @UseGuards(FirebaseAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Delete(':id')
   public async remove(@Param('id') id: string) {
     return this.themesService.remove(id);

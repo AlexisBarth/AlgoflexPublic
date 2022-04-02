7bf0ab068930ff76b4f28f082a57905ff6a9c611
PichardN <nicolas.pichard@epitech.eu>
2022-04-02 15:46:40 +0200

    Add browser router to use link

--- algoflex_frontend/src/index.tsx
+++ algoflex_frontend/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Firebase, { FirebaseContext } from './services/Firebase';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseContext.Provider value={new Firebase()}>
-      <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')

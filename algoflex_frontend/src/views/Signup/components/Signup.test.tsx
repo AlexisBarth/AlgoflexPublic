d523b5f7162bf4cd701c1f58b83fa4032b917a1a
PichardN <nicolas.pichard@epitech.eu>
2022-04-02 15:52:32 +0200

    Reduce TU

--- algoflex_frontend/src/views/Signup/components/Signup.test.tsx
+++ algoflex_frontend/src/views/Signup/components/Signup.test.tsx
@@ -1,9 +1,3 @@
-import Signup from './Signup'
-import {render} from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 test.todo("Make Signup tests");
-
-test('load component Signup', async () => {
-    render(<Signup/>)
-});
